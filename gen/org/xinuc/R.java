d2edecb6dff15ea3d3511686c0d30edb0539997a
Nugroho Herucahyono <xinuc@xinuc.org>
2011-12-11 00:58:31 +0700

    create new layout

--- gen/org/xinuc/R.java
+++ gen/org/xinuc/R.java
@@ -10,15 +10,24 @@ package org.xinuc;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int background=0x7f030000;
+    }
+    public static final class id {
+        public static final int about_button=0x7f050002;
+        public static final int continue_button=0x7f050000;
+        public static final int exit_button=0x7f050003;
+        public static final int new_button=0x7f050001;
+    }
     public static final class layout {
         public static final int main=0x7f020000;
     }
     public static final class string {
-        public static final int about_label=0x7f030004;
-        public static final int app_name=0x7f030000;
-        public static final int continue_label=0x7f030002;
-        public static final int exit_label=0x7f030005;
-        public static final int main_title=0x7f030001;
-        public static final int new_game_label=0x7f030003;
+        public static final int about_label=0x7f040004;
+        public static final int app_name=0x7f040000;
+        public static final int continue_label=0x7f040002;
+        public static final int exit_label=0x7f040005;
+        public static final int main_title=0x7f040001;
+        public static final int new_game_label=0x7f040003;
     }
 }
